9b9c5b81519e93bfed4d2a9774fdaeacfa56ea32
JesusMorales0858 <jymm0858@gmail.com>
2023-08-31 09:37:05 -0300

    second commit 31/08/23 09:37

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
-  <nav>
+ <!-- <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <router-link to="/gestionModulos">Gestion de modulos</router-link>|
+  </nav>-->
+  <nav class="navbar bg-dark navbar-dark">
+  <div class="container-fluid">
+    <div class="navbar-brand">
+      <i class="bi bi-person rounded-pill" style="color: antiquewhite;"></i><!--ajustar icono user-->
+      Usuario</div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Inicio</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/gestionModulos" class="nav-link">Gestion de modulos</router-link>
+        </li>
+        <li>
+          <p class="nav-link">Modulos creados</p><!--configurar-->
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>  
   <router-view/>
 </template>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
+  text-align: center;
 }
 
 nav a {
@@ -26,5 +48,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+}*/
 </style>
