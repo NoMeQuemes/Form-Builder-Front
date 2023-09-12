840f91c9493073cc50a7518a86aae92933c745e1
Lumila <lmontana@excelenciadigital.net>
2023-09-12 16:49:04 -0300

    12-09-2023

--- src/components/FieldComponent.vue
+++ src/components/FieldComponent.vue
@@ -1,33 +1,40 @@
-<!-- El componente FieldComponent se encarga de la logica
-    para mostrar los campos dinámicos según su tipo. -->
+
+
 <template>
-    <div class="form-group"> <!-- aqui podemos agregar clases de bootstrap para definir estilos -->
-      <label style="margin-right: 10px;">{{ field.label }}</label>
-      <input v-if="field.type === 'text'" type="text" :placeholder="field.placeholder" v-model="field.value" class="form-control">
-      <input v-if="field.type === 'textarea'" type="textarea" :placeholder="field.placeholder" v-model="field.value" class="form-control">
-      <input v-if="field.type === 'password'" type="password" :placeholder="field.placeholder" v-model="field.value" class="form-control">
-      <input v-if="field.type === 'email'" type="email" :placeholder="field.placeholder" v-model="field.value" class="form-control">
-      <input v-if="field.type === 'number'" type="number" :placeholder="field.placeholder" v-model="field.value" class="form-control">
-      <select v-else-if="field.type === 'select'" v-model="field.value" class="form-control">
-        <option v-for="option in field.options" :value="option">{{ option }}</option>
-      </select>
-      <input v-else-if="field.type === 'checkbox'" type="checkbox" v-model="field.value" class="form-check-input">
-      <input v-else-if="field.type === 'submit'" type="submit" value="Guardar" class="btn btn-primary">
-      <input v-else-if="field.type === 'reset'" type="reset" class="btn btn-primary">
-      <!-- Agrega más condiciones para otros tipos de campos -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: ['field']
-  };
-  </script>
-  <style scoped>
- input{
+  <div class="form-group">
+    <label style="margin-right: 10px;">{{ field.label }}</label>
+    <input
+      v-if="field.type === 'text' || field.type === 'password' || field.type === 'email' || field.type === 'number'"
+      :type="field.type"
+      :placeholder="field.placeholder"
+      v-model="field.value"
+      class="form-control"
+    >
+    <textarea v-else-if="field.type === 'textarea'"
+      :placeholder="field.placeholder"
+      v-model="field.value"
+      class="form-control"
+    ></textarea>
+    <select v-else-if="field.type === 'select'" v-model="field.value" class="form-control">
+      <option v-for="option in field.options" :value="option">{{ option }}</option>
+    </select>
+    <input v-else-if="field.type === 'checkbox'" type="checkbox" v-model="field.value" class="form-check-input">
+    <input v-else-if="field.type === 'submit' || field.type === 'reset'" :type="field.type" :value="field.label" class="btn btn-primary">
+    <!-- Agrega más condiciones para otros tipos de campos -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['field']
+};
+</script>
+
+<style scoped>
+input {
   margin-bottom: 5px;
- }
- select{
+}
+select {
   margin-bottom: 5px;
- }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import '@/assets/bootstrap.min.css'
 import '@/assets/bootstrap.bundle.min.js'
 import axios from "axios";
 import VueAxios from "vue-axios";
+import BootstrapIconsPlugin from 'bootstrap-icons-vue';
+
 
 import BootstrapVue from 'bootstrap-vue'
-createApp(App).use(VueAxios, axios).use(store).use(router).mount('#app');
+createApp(App).use(VueAxios, axios).use(store).use(router).use(BootstrapIconsPlugin).mount('#app');
 //Vue.use(BToast);
